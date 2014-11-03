22250d8ae0de5892b93b8190d2f551ce3b70644f
taesongkim <taesongkim@gmail.com>
2014-11-03 16:18:10 -0500

    zen-ified colors and added a tie game response

--- tictactoe.js
+++ tictactoe.js
@@ -10,9 +10,67 @@ var board = {
   'Pos6': 9,
   'Pos7': 10,
   'Pos8': 11,
-  'turnCount': 0
 };
 
+var checkForWin = function () {  
+  if (
+     (board.Pos0 == board.Pos1 && board.Pos1 == board.Pos2) ||
+     (board.Pos3 == board.Pos4 && board.Pos4 == board.Pos5) ||
+     (board.Pos6 == board.Pos7 && board.Pos7 == board.Pos8) ||
+     (board.Pos0 == board.Pos3 && board.Pos3 == board.Pos6) ||
+     (board.Pos1 == board.Pos4 && board.Pos4 == board.Pos7) ||
+     (board.Pos2 == board.Pos5 && board.Pos5 == board.Pos8) ||
+     (board.Pos0 == board.Pos4 && board.Pos4 == board.Pos8) ||
+     (board.Pos2 == board.Pos4 && board.Pos4 == board.Pos6)
+     ) {
+    return true;
+  } else return false;  
+};
+
+var turnCount = 0;
+
+var currentPlayer = 1;
+
+$('.tttrow').bind('click', function () {
+  $(this).css('background', '#706e6e');
+  var iconElement = $(this).children('i');
+  var position = $(this).attr('id')
+
+  if($(this).hasClass('played')) { //already chosen
+    alert("Looks like that's been selected");
+  } 
+  else { //not yet played
+    $(this).addClass('played');
+    board['Pos' + position] = currentPlayer;
+  
+
+    if (currentPlayer === 1) {
+      iconElement.addClass('fa-circle-o');
+      if (checkForWin()) {
+        alert('Player' + currentPlayer + ' has won the game!');
+        location.reload();
+      }
+      currentPlayer = 2;
+    }
+
+    else {
+      iconElement.addClass('fa-close');
+      if (checkForWin()) {
+        alert('Player' + currentPlayer + ' has won the game!');
+        location.reload();
+      }
+      currentPlayer = 1;
+    }
+
+    turnCount ++;
+    
+  }
+  if (turnCount == 9) {
+    alert("Looks like it's a tie! Lets try again.");
+    location.reload();
+  };
+  console.log(turnCount)
+});
 
 
 // var Board = [Pos0, Pos1, Pos2, Pos3, Pos4, Pos5, Pos6, Pos7, Pos8];
@@ -39,59 +97,6 @@ var board = {
 //   playerXChoice ();
 // };
 
-//click events
-var currentPlayer = 1;
-
-$('.tttrow').bind('click', function () {
-  $(this).css('background', 'red');
-  var iconElement = $(this).children('i');
-  var position = $(this).attr('id')
-
-  if($(this).hasClass('played')) { //already chosen
-    alert("Looks like that's been selected");
-  } 
-  else { //not yet played
-    $(this).addClass('played');
-    board['Pos' + position] = currentPlayer;
-  }
-
-  if (currentPlayer === 1) {
-    iconElement.addClass('fa-circle-o');
-    if (checkForWin()) {
-      alert('Player' + currentPlayer + ' has won the game!');
-      location.reload();
-    }
-    currentPlayer = 2;
-  }
-
-  else {
-    iconElement.addClass('fa-close');
-    if (checkForWin()) {
-      alert('Player' + currentPlayer + ' has won the game!');
-      location.reload();
-    }
-    currentPlayer = 1;
-  }
-
-
-});
-
-
-
-var checkForWin = function () {  
-  if (
-     (board.Pos0 == board.Pos1 && board.Pos1 == board.Pos2) ||
-     (board.Pos3 == board.Pos4 && board.Pos4 == board.Pos5) ||
-     (board.Pos6 == board.Pos7 && board.Pos7 == board.Pos8) ||
-     (board.Pos0 == board.Pos3 && board.Pos3 == board.Pos6) ||
-     (board.Pos1 == board.Pos4 && board.Pos4 == board.Pos7) ||
-     (board.Pos2 == board.Pos5 && board.Pos5 == board.Pos8) ||
-     (board.Pos0 == board.Pos4 && board.Pos4 == board.Pos8) ||
-     (board.Pos2 == board.Pos4 && board.Pos4 == board.Pos6)
-     ) {
-    return true;
-  } else return false;  
-};
 
 // for (i=0; i<9; i++) {
 //   if (i%2 == 0) {
